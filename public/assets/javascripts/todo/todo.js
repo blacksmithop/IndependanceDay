3b0c8ffd02c3d942e3feacb1b1ec069db8a830f1
Abhinav <you@example.com>
2021-07-22 20:19:39 +0530

    css changes

--- public/assets/javascripts/todo/todo.js
+++ public/assets/javascripts/todo/todo.js
@@ -45,20 +45,37 @@ $(document).ready(function () {
 
 
 // Keep track of todo - (move to Promises)
-$(document).on('click', 'span', function () {
-  let item = this.innerHTML;
-  
+$(document).on('click', 'label', function (e) {
+  let label = this
+  let span = $(this).find('span')[0];
+  let checkbox = $(this).find('checkbox')[0];
+  let item = span.innerHTML;
+  let cname = span.className;
   let taskValidate = new Promise(function (resolve, reject) {
-    let x = 0;
+    if (cname == "locked"){
+      reject("Locked checkbox, ignoring");
+    }
+
     if (!(completed.includes(item))){
         completed.push(item);
-        console.log(item);
+        $(label).css({"color":"586186", "text-decoration": "line-through"});
+        $(checkbox).prop('checked', true);
     }
     else {
-      throw("To Do already marked!");
+      var index = completed.indexOf(item);
+      if (index >= 0) {
+        completed.splice( index, 1 );
+      }
+      console.log(`Length: ${completed.length}`);
+      $(checkbox).prop('checked', false);
+      $(label).css({"color":"", "text-decoration": ""});
+      throw("Removing from list");
+
     }
+    
     // some code (try to change x to 5)
     if (completed.length == 5) {
+      console.log(`Length: ${completed.length}`);
       resolve(`You have done 5 tasks!`);
     }
     else {
@@ -72,13 +89,15 @@ $(document).on('click', 'span', function () {
   ).catch(function(error){
       console.log(error);
     });
-
+    return false;
 });
 
 
 function populateTodo() {
   todoArray.forEach(function (todoArr) {
-    var item = `<li><label><input type="checkbox" ${todoArr.completed?"checked disabled":""}><i></i><span>${capFirst(todoArr.title)}</span><a href='#'>–</a></label></li>`
+    var done = todoArr.completed?"checked disabled":"";
+    var locked = todoArr.completed?"locked":"unlocked";
+    var item = `<li><label><input type="checkbox" ${done}><i></i><span class='${locked}'>${capFirst(todoArr.title)}</span><a href='#'>–</a></label></li>`
     $('.todo').append(item);
   });
 }
