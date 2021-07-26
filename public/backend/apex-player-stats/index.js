7e891626696d022111f7e81a2edb15aad575ed3b
Abhinav <you@example.com>
2021-07-26 13:43:03 +0530

    port

--- public/backend/apex-player-stats/index.js
+++ public/backend/apex-player-stats/index.js
@@ -4,19 +4,6 @@ const axios = require('axios').default
 const app = express()
 
 
-
-/*async function getPlayerDetails(username, platform) {
-    console.log(`User: ${username} (${platform})`)
-    let URL = `https://api.mozambiquehe.re/bridge?version=5&platform=${platform}&player=${username}&auth=${process.env.apexKey}`;
-    await axios
-      .get(URL)
-      .then(function (response) {
-        // handle success
-        console.log(`Status: ${response.status}`)
-        console.log(response.data)
-        return response.data
-      })
-  }*/
 const getPlayerDetails = async (username, platform) => {
   try {
       const URL = `https://api.mozambiquehe.re/bridge?version=5&platform=${platform}&player=${username}&auth=${process.env.apexKey}`;
@@ -41,5 +28,5 @@ app.get('/stats/:username/:platform', async (req, res) => {
 
 
 app.listen(process.env.PORT, () => {
-  console.log(`App listening at http://localhost:${process.env.PORT}`)
+  console.log(`App listening at http://localhost:3000`)
 })
