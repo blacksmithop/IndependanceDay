8987b3df61f4cbb41a2ab4028d8a0d96ba6deafb
Abhinav <you@example.com>
2021-07-30 09:09:45 +0530

    link

--- public/backend/apex-player-stats/index.js
+++ public/backend/apex-player-stats/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+  //res.setHeader('Access-Control-Allow-Origin', 'https://blacksmithop.gitlab.io/full-stack-certification/pages/apex-player-stats/');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
