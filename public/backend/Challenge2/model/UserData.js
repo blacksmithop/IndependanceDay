46c2bffec53463d2a8470adc8e490fe35f12b0b7
Abhinav <you@example.com>
2021-08-15 14:41:49 +0530

    IndependanceDay

---
+++ public/backend/Challenge2/model/UserData.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+var dotenv = require('dotenv');
+
+// Load .env
+dotenv.config({path: "./model/.env"});
+const pwd = process.env.password;
+const uri = `mongodb+srv://BlackSmithOP:${pwd}@cluster0.fk3pw.mongodb.net/Tests`;
+
+
+mongoose 
+ .connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,   })   
+ .then(() => console.log("Database connected!"))
+ .catch(err => console.log(err));
+ 
+console.log(uri);
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    name: String,
+    email : String,
+});
+
+var UserData = mongoose.model('user',UserSchema);
+
+module.exports = UserData;
